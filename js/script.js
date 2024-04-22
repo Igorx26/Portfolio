55f362391becce219723e481401f464e7d29794c
Igor Martins Silva <igor12-04@hotmail.com>
2024-04-22 19:21:23 -0300

    22042024

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ toggleIcons.forEach(toggle => {
     toggle.classList.add('disabled')
     setTimeout(() => {
       toggle.classList.remove('disabled')
-    }, 1500)
+    }, 2500)
 
     icons.forEach(icon => {
       icon.classList.toggle('bx-sun')
@@ -28,4 +28,77 @@ toggleIcons.forEach(toggle => {
     darkContainer.classList.toggle('active')
 
   })
+
+})
+
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x')
+  navbar.classList.toggle('active')
+}
+
+
+// Destaque nav onscroll
+
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY
+    let offset = sec.offsetTop - 150
+    let height = sec.offsetHeight
+    let id = sec.getAttribute('id')
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('destaque')
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('destaque')
+        links.classList.remove('destaque')
+        document.querySelector('#dark-container header nav a[href*=' + id + ']').classList.add('destaque')
+      })
+    }
+  })
+
+  // sticky navbar
+  let header = document.querySelector('header')
+
+  header.classList.toggle('sticky', window.scrollY > 100)
+
+  // remove toggle icon and navbar when click navbar link (scroll)
+  menuIcon.classList.remove('bx-x')
+  navbar.classList.remove('active')
+}
+
+// scroll reveal
+ScrollReveal({
+  // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .habilidades-container, .portfolio-box, .contatos form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .sobre-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .sobre-content', { origin: 'right' });
+
+// typed js
+const typed = new Typed('.multiple-text', {
+  strings: ['Desenvolvedor Frontend'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+})
+
+const typeds = new Typed('#dark-container .multiple-text', {
+  strings: ['Desenvolvedor Frontend'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
 })
